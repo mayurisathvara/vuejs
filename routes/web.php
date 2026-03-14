81fae6c23c58bf290c52b1495836d90764134d1f
mayurisathvara <mayuriparmar0299@gmail.com>
2026-03-14 22:56:33 +0530

    coe

--- routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-// SPA - Catch all routes and return the main view
-Route::get('/{any}', function () {
-    return view('welcome');
-})->where('any', '.*');
